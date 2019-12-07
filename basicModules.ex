e104d20539b72ccf96bf2cffb3d6f57f03133326
Colin <cstatro@gmail.com>
2019-12-07 17:25:54 -0500

    putting the basic maps examples in

---
+++ basicModules.ex
@@ -0,0 +1,4 @@
+defmodule Rectangle do
+    defstruct height: 0,length: 0
+    
+end
---
+++ maps.ex
@@ -0,0 +1,8 @@
+map = %{jeb: "jeb", bush: "bush"}
+
+# update a map
+%{map | jeb: "Jeb!"}
+
+#add a key
+
+Map.put(map,:electoral_votes,"all of em")
